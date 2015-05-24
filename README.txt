b3671da01d3d3de171b8d835e983499988d90aaf
Marie Martin <martimar@etu.utc.fr>
2015-05-24 13:00:12 +0200

    Suppression aa.txt / Ajout classes Activite, Rdv, Reunion dans calendar.h

---
+++ README.txt
@@ -0,0 +1,6 @@
+Projet d'application C++/Qt :
+* gestion d'agenda, emploi du temps hebdomadaire
+* gestion de projets
+
+Remarques : 
+* Tache : classe abstraite?
--- calendar.h
+++ calendar.h
@@ -5,8 +5,11 @@
 #include <QTextStream>
 #include <vector>
 #include "timing.h"
-using namespace std;
 
+using namespace std;
+using namespace TIME;
+
+//******************************************************************************************
 class CalendarException
 {
     QString info;
@@ -15,19 +18,45 @@ public:
     QString getInfo() const { return info; }
 };
 
+QTextStream& operator<<(QTextStream& f, const Duree & d);
+QTextStream& operator>>(QTextStream&, Duree&); //lecture format hhHmm
 
-QTextStream& operator<<(QTextStream& f, const TIME::Duree & d);
-QTextStream& operator>>(QTextStream&, TIME::Duree&); //lecture format hhHmm
+//******************************************************************************************
+class Event { // CLASSE ABSTRAITE
 
+public:
+};
+
+class Activite: public Event {
+    QString id;
+    QString titre;
+    Duree duree;
+    QString lieu;
+public:
+    const QString& getId() const { return id; }
+    const QString& getTitre() const { return titre; }
+    const Duree& getDuree() const { return duree; }
+    const QString& getLieu() const { return lieu; }
+};
+
+class Rdv : public Event {
+
+};
+
+class Reunion : public Event {
+
+};
+
+//******************************************************************************************
 class Tache
 {
  private:
     QString identificateur;
     QString titre;
-    TIME::Duree duree;
+    Duree duree;
     QDate disponibilite;
     QDate echeance;
-protected: Tache(const QString& id, const QString& t, const TIME::Duree& dur, const QDate& dispo, const QDate& deadl, bool preempt=false):
+protected: Tache(const QString& id, const QString& t, const Duree& dur, const QDate& dispo, const QDate& deadl, bool preempt=false):
             identificateur(id),titre(t),duree(dur),disponibilite(dispo),echeance(deadl){}
     Tache(const Tache& t);
     Tache& operator=(const Tache&);
@@ -37,8 +66,8 @@ public:
     void setId(const QString& str);
     QString getTitre() const { return titre; }
     void setTitre(const QString& str) { titre=str; }
-    TIME::Duree getDuree() const { return duree; }
-    void setDuree(const TIME::Duree& d) { duree=d; }
+    Duree getDuree() const { return duree; }
+    void setDuree(const Duree& d) { duree=d; }
     QDate getDateDisponibilite() const {  return disponibilite; }
     QDate getDateEcheance() const {  return echeance; }
     void setDatesDisponibiliteEcheance(const QDate& disp, const QDate& e)
@@ -51,29 +80,13 @@ public:
 
 };
 
-//******************************************************************************************
-class Event // CLASSE ABSTRAITE
-{
-
-public:
-};
-
-
-class Activite: public Event
-{
-    QString type;
-public:
-
-};
-
-//******************************************************************************************
 class TacheU : private Tache , public Event
 {
     bool preemptive;
 
  public:
 
-    TacheU(const QString& id, const QString& t, const TIME::Duree& dur, const QDate& dispo, const QDate& deadline, bool p=false):
+    TacheU(const QString& id, const QString& t, const Duree& dur, const QDate& dispo, const QDate& deadline, bool p=false):
         Tache(id,t,dur,dispo,deadline), preemptive(p){}
     TacheU(Tache& t, bool p=false):Tache(t.getId(),t.getTitre(),t.getDuree(),t.getDateDisponibilite(),t.getDateEcheance()),preemptive(p){}
 
@@ -84,14 +97,13 @@ class TacheU : private Tache , public Event
 
 };
 
-
 class TacheC : public Tache
 {
     typedef std::vector<Tache> vectcomp;
     vectcomp tachescomp;
 
 public:
-    TacheC(const QString& id, const QString& t, const TIME::Duree& dur, const QDate& dispo, const QDate& deadl): Tache(id,t,dur,dispo,deadl)
+    TacheC(const QString& id, const QString& t, const Duree& dur, const QDate& dispo, const QDate& deadl): Tache(id,t,dur,dispo,deadl)
     {tachescomp.reserve(10);}
     bool Precedence(const Tache& t);
     void ajoutTache(const Tache& t);
@@ -116,7 +128,7 @@ class VPrincipale // class abstraite pour le tableau de taches
     virtual ~VPrincipale();
     VPrincipale(const VPrincipale& m);
     VPrincipale& operator=(const VPrincipale& m);
-    TacheU& ajouterTacheU(const QString& id, const QString& t, const TIME::Duree& dur, const QDate& dispo, const QDate& deadline, bool preempt=false);
+    TacheU& ajouterTacheU(const QString& id, const QString& t, const Duree& dur, const QDate& dispo, const QDate& deadline, bool preempt=false);
     Tache& getTache(const QString& id);
     bool isTacheExistante(const QString& id) const { return trouverTache(id)!=0; }
     const Tache& getTache(const QString& code) const;
@@ -125,6 +137,7 @@ class VPrincipale // class abstraite pour le tableau de taches
     virtual void afficher(QTextStream& f) const =0;
 
 };
+
 //******************************************************************************************
 class Projet: public VPrincipale // On herite de l'interface et du comportement
 {
@@ -157,6 +170,7 @@ class Projet: public VPrincipale // On herite de l'interface et du comportement
     }
 
 };
+
 class ProjetManager: public VPrincipale
 {
     typedef std::vector<Projet*> TabProjet;
@@ -178,8 +192,6 @@ public:
     }
 
 };
-
-
 
 //******************************************************************************************
 class TacheManager: public VPrincipale // On herite de l'interface et du comportement
@@ -204,10 +216,7 @@ class TacheManager: public VPrincipale // On herite de l'interface et du comport
 
 };
 
-
 //******************************************************************************************
-
-
 class Programmation
 {
     const Event* events;
@@ -236,8 +245,6 @@ public:
 };
 
 //******************************************************************************************
-
-
 
 #endif // CALENDAR_H
 
